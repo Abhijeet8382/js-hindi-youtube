d033f59c97fe0513783bf415984b4639ae114870
Abhijeet8382 <abhijeet838287@gmail.com>
2025-10-03 18:02:26 +0000

    conversion of data types

---
+++ 1basics/3conversionoperation.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+
+ 
+// console.log(typeof score);
+
+let ValueInNumber =Number (score)
+
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+
+// "33" => 33
+// "33abc" =>nan
+// true=> 1 ; false -0
+
+let isLoggeedIn = 1
+
+let booleanisLoggeedIn = Boolean(isLoggeedIn)
+// console.log(booleanisLoggeedIn);
+
+
+// 1 => true ; 0=> false 
+// "" => false
+//"abhijeet" =>true
+
+let someNumber = 33
+
+let stringNumber=String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
